b33c2a0ba31c0d11711bcc1e0bd8f97f43eab036
revanthkunche1206 <155340253+revanthkunche1206@users.noreply.github.com>
2024-08-31 12:25:34 +0530

    Update sub-task-3.rs

--- task-03/Rust/Sub-task 3/sub-task-3.rs
+++ task-03/Rust/Sub-task 3/sub-task-3.rs
@@ -1,13 +1,13 @@
 use std::io;
 
 fn main() {
-    // Read the value of n from the user
+    
     let mut input = String::new();
     println!("Enter the value of n: ");
     io::stdin().read_line(&mut input).expect("Failed to read line");
     let n: usize = input.trim().parse().expect("Please enter a valid number");
 
-    // First pattern
+    
     for i in 0..n {
         for _ in i..n {
             print!("  ");
@@ -21,7 +21,7 @@ fn main() {
         println!();
     }
 
-    // Second pattern
+    
     for i in 0..=n {
         for _ in 0..i {
             print!("  ");
